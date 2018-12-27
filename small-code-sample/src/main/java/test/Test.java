c76bfaa71c34973db66070b4cf533b48f8f44e34
fifadxj <dxjwy1@sina.com>
2018-12-27 15:00:35 +0800

    no message

--- small-code-sample/src/main/java/test/Test.java
+++ small-code-sample/src/main/java/test/Test.java
@@ -1,6 +1,7 @@
 package test;
 
 import org.apache.commons.io.IOUtils;
+import sample.datastructure.DigitTransform;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -14,20 +15,60 @@ import java.util.Random;
  */
 public class Test {
     public static void main(String[] args) throws IOException {
-        String i = new Random().nextInt() + "-VALUE";
-        System.out.println(i);
-        System.out.println(i.hashCode());
-        System.out.println(i.hashCode() % 64 / 16);
-        System.out.println(i.hashCode() % 64);
+//        long value = 259753824552722432L;
+//        long value2 = value >> 12 << 12 | 0001;
+//        long value3 = value << 52 << 52;
+//        System.out.println(value);
+//        System.out.println(value2);
+//        System.out.println(value3);
+//        System.out.println(Long.toBinaryString(value));
+//        System.out.println(Long.toBinaryString(value2));
+//
+//        if (true) return;
 
-//        i = "1828";
+ //        String i = new Random().nextInt() + "-VALUE";
+//        System.out.println(i);
 //        System.out.println(i.hashCode());
+//        System.out.println(i.hashCode() % 64 / 16);
 //        System.out.println(i.hashCode() % 64);
+//
+////        i = "1828";
+////        System.out.println(i.hashCode());
+////        System.out.println(i.hashCode() % 64);
+//
+//        String logicSQL = IOUtils.toString(Test.class.getResourceAsStream("/hello.txt"));
+//        System.out.println(System.getProperty("line.separator"));
+//        System.out.println(logicSQL);
+//        String format = logicSQL.replaceAll(System.getProperty("line.separator"), " ");
+//        System.out.println(format);
 
-        String logicSQL = IOUtils.toString(Test.class.getResourceAsStream("/hello.txt"));
-        System.out.println(System.getProperty("line.separator"));
-        System.out.println(logicSQL);
-        String format = logicSQL.replaceAll(System.getProperty("line.separator"), " ");
-        System.out.println(format);
+        //259753824552722432L
+        long orignal = 280007445634592767L;
+
+        String a = Long.toBinaryString(orignal);
+        int prefix = 64 - a.length();
+        StringBuilder sb = new StringBuilder();
+        System.out.println(prefix);
+        for (int i = 0; i < prefix; i++) {
+            sb.append(0);
+        }
+
+        String binary = sb.append(Long.toBinaryString(orignal)).toString();
+        System.out.println(binary);
+
+        long sum = 0L;
+        for (int i = 0 ; i < binary.length(); i++) {
+            //System.out.print(i + ",");
+            //char c = binary.charAt(i);
+            //System.out.print(c);
+            String s = binary.substring(i, i + 1);
+            int ii = Integer.valueOf(s);
+            //System.out.print(ii + ",");
+            //System.out.print(ii * Math.pow(2, i) + ",");
+            sum += ii * Math.pow(2, binary.length() - i - 1);
+        }
+        System.out.println(sum);
+//        Long b = Long.valueOf((long)Math.pow(2, 63) - 1);
+//        System.out.println(b);
     }
 }
