21883fa30713a0ef305254c4ba5ca0a83cbcb861
daixiaojun <daixiaojun@innotechx.com>
2021-02-04 17:47:24 +0800

    no message

--- small-code-sample/src/main/java/test/Test.java
+++ small-code-sample/src/main/java/test/Test.java
@@ -1,19 +1,11 @@
 package test;
 
-import org.apache.commons.io.IOUtils;
-import sample.datastructure.DigitTransform;
+import com.google.common.primitives.Ints;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.net.SocketAddress;
-import java.util.Date;
-import java.util.Objects;
-import java.util.Random;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.LockSupport;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by za-daixiaojun on 2017/12/20.
@@ -23,80 +15,81 @@ public class Test {
     public static int n = 0;
 
     public static void main(String[] args) throws Exception {
-        //lock();
-        sync();
+        System.out.println(tableSizeFor(8));
     }
 
-    public static void lock() throws Exception {
-        final CountDownLatch latch = new CountDownLatch(2);
-        final Lock lock = new ReentrantLock();
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 100000; i++) {
-                    lock.lock();
-                    try {
-                        n++;
-                    } finally {
-                        lock.unlock();
-                    }
-                }
-                latch.countDown();
-            }
-        });
-        Thread t2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 100000; i++) {
-                    lock.lock();
-                    try {
-                        n++;
-                    } finally {
-                        lock.unlock();
-                    }
-                }
-                latch.countDown();
-            }
-        });
-        t.start();
-        t2.start();
-
-        long start = System.currentTimeMillis();
-        latch.await();
-        System.out.println(n);
-        System.out.println("cost " + (System.currentTimeMillis() - start) + " ms");
+    static final int tableSizeFor(int cap) {
+        int MAXIMUM_CAPACITY = 1 << 30;
+        System.out.println(MAXIMUM_CAPACITY);
+        int n = cap - 1;
+        n |= n >>> 1;
+        n |= n >>> 2;
+        n |= n >>> 4;
+        n |= n >>> 8;
+        n |= n >>> 16;
+        return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
     }
 
-    public static void sync() throws Exception {
-        final CountDownLatch latch = new CountDownLatch(2);
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 100000; i++) {
-                    synchronized (Test.class) {
-                        n++;
-                    }
-                }
-                latch.countDown();
-            }
-        });
-        Thread t2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 100000; i++) {
-                    synchronized (Test.class) {
-                        n++;
-                    }
-                }
-                latch.countDown();
-            }
-        });
-        t.start();
-        t2.start();
+    public static long count() {
+        long count = 0;
+        for (long i = 0; i < 500000000; i++) {
+            count++;
+        }
 
-        long start = System.currentTimeMillis();
-        latch.await();
-        System.out.println(n);
-        System.out.println("cost " + (System.currentTimeMillis() - start) + " ms");
+        return count;
     }
+
+    public static long count2() {
+        long count;
+        synchronized (Test.class) {
+            count = 0;
+            for (long i = 0; i < 500000000; i++) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+
+    public static String encode(String str) {
+        String prefix = "\\u";
+        StringBuffer sb = new StringBuffer();
+        char[] chars = str.toCharArray();
+        if (chars == null || chars.length == 0) {
+            return null;
+        }
+        for (char c : chars) {
+            sb.append(prefix);
+            sb.append(Integer.toHexString(c));
+        }
+        return sb.toString();
+    }
+
+    public static String decode(String str) {
+        String sg = "\\u";
+        int a = 0;
+        List<String> list = new ArrayList<>();
+        while (str.contains(sg)) {
+            str = str.substring(2);
+            String substring;
+            if (str.contains(sg)) {
+                substring = str.substring(0, str.indexOf(sg));
+            } else {
+                substring = str;
+            }
+            if (str.contains(sg)) {
+                str = str.substring(str.indexOf(sg));
+            }
+            list.add(substring);
+        }
+        StringBuffer sb = new StringBuffer();
+        if (! (list.size() == 0)){
+            for (String string : list) {
+                sb.append((char) Integer.parseInt(string, 16));
+            }
+        }
+        return sb.toString();
+    }
+
 }
